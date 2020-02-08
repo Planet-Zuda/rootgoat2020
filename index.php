7dc6894ae040ee63b39c9388edf0786e2ee4fcdc
Planet-Zuda <60828472+Planet-Zuda@users.noreply.github.com>
2020-02-08 13:26:51 -0800

    The first ever unhackable software
    Can't hack this! #rootgoat2020

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+echo "Planetzuda.com is proud to release this unhackable program. If you can hack this file in the first hour of its release and not the underlying language or compiler or outdated versions, the first person to do so will get 1000 dollars.";
+echo "#rootgoat2020";
+?>
